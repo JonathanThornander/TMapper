e3542903b8b7e0002b061479edd12976c6d4e7a8
Jonathan Thornander <jonathant@centiro.se>
2023-10-18 10:58:13 +0200

    Add project files.

---
+++ TMapper/IMapper.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace TMapper
+{
+    /// <summary>
+    /// Maps the source type to the target type asynchronously
+    /// </summary>
+    /// <typeparam name="TSource">The object to map from</typeparam>
+    /// <typeparam name="TTarget">The object to map to</typeparam>
+    public interface IMapper<TSource, TTarget>
+    {
+        public Task<TTarget> Map(TSource source, CancellationToken cancellationToken = default);
+    }
+}
